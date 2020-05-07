6bdcb9dc9e1620a6822dca304ffbce125e93ec76
dev <amour-youcef@hotmail.com>
2020-05-08 01:32:41 +0200

    08052020

---
+++ IntroJPA/src/fr/koor/webstore/business/Article.java
@@ -0,0 +1,58 @@
+
+package fr.koor.webstore.business;
+
+public class Article {
+
+    private int idArticle;
+    private String description;
+    private String brand;
+    private double price;
+    
+    
+    public Article() {
+        this( "unknown", "unknown", 0 );
+    }
+    
+    
+    public Article( String description, String brand, double price ) {
+        this.setDescription( description );
+        this.setBrand( brand );
+        this.setPrice( price );
+    }
+
+
+    public int getIdArticle() {
+        return idArticle;
+    }
+        
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String getBrand() {
+        return brand;
+    }
+    
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    @Override
+    public String toString() {
+        return "Article [idArticle=" + idArticle + ", description=" + description +
+               ", brand=" + brand + ", price=" + price + "]";
+    }
+    
+}
