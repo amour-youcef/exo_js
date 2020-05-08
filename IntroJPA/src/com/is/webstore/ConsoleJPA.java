3e7a2609aa5f241b339b2bb3cdb50098058e24d1
dev <amour-youcef@hotmail.com>
2020-05-08 02:58:55 +0200

    08052020

--- IntroJPA/src/com/is/webstore/ConsoleJPA.java
+++ IntroJPA/src/com/is/webstore/ConsoleJPA.java
@@ -18,47 +18,47 @@ public class ConsoleJPA {
             entityManagerFactory = Persistence.createEntityManagerFactory("WebStore");
             entityManager = entityManagerFactory.createEntityManager();
         
-            System.out.println( "- Lecture de tous les articles -----------" );
-            
-            List<Article> articles = entityManager.createQuery( "from Article", Article.class )
-                                   .getResultList();
-            for (Article article : articles) {
-                System.out.println( article );
-            }
-            
-            System.out.println( "- Insertion d'un nouvel article ----------" );
-            
-            EntityTransaction trans = entityManager.getTransaction();
-            trans.begin();
-            
-            Article newArticle = new Article( "DBS", "Aston Martin", 200000 );
-            entityManager.persist( newArticle );
-    
-            List<Article> results = entityManager.createQuery("from Article", Article.class).getResultList();
-            for( Article article : results) {
-                System.out.println( article );
-            }
-    
-            System.out.println( "- Modification d'un article --------------" );
-        
-            newArticle.setPrice( 40000 );
-            entityManager.persist( newArticle );
-            
-            results = entityManager.createQuery("from Article", Article.class).getResultList();
-            for( Article article : results) {
-                System.out.println( article );
-            }
-    
-            System.out.println( "- Suppression d'un article ---------------" );
-            
-            entityManager.remove( newArticle );
-            
-            results = entityManager.createQuery("from Article", Article.class).getResultList();
-            for( Article article : results) {
-                System.out.println( article );
-            }
-                    
-            trans.commit();
+//            System.out.println( "- Lecture de tous les articles -----------" );
+//            
+//            List<Article> articles = entityManager.createQuery( "from Article", Article.class )
+//                                   .getResultList();
+//            for (Article article : articles) {
+//                System.out.println( article );
+//            }
+//            
+//            System.out.println( "- Insertion d'un nouvel article ----------" );
+//            
+//            EntityTransaction trans = entityManager.getTransaction();
+//            trans.begin();
+//            
+//            Article newArticle = new Article( "DBS", "Aston Martin", 200000 );
+//            entityManager.persist( newArticle );
+//    
+//            List<Article> results = entityManager.createQuery("from Article", Article.class).getResultList();
+//            for( Article article : results) {
+//                System.out.println( article );
+//            }
+//    
+//            System.out.println( "- Modification d'un article --------------" );
+//        
+//            newArticle.setPrice( 40000 );
+//            entityManager.persist( newArticle );
+//            
+//            results = entityManager.createQuery("from Article", Article.class).getResultList();
+//            for( Article article : results) {
+//                System.out.println( article );
+//            }
+//    
+//            System.out.println( "- Suppression d'un article ---------------" );
+//            
+//            entityManager.remove( newArticle );
+//            
+//            results = entityManager.createQuery("from Article", Article.class).getResultList();
+//            for( Article article : results) {
+//                System.out.println( article );
+//            }
+//                    
+//            trans.commit();
         } finally {
             if ( entityManager != null ) entityManager.close();
             if ( entityManagerFactory != null ) entityManagerFactory.close();
