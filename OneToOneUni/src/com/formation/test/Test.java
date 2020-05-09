7d2678a19685b38b8586d3f378a1e470565af2c5
dev <amour-youcef@hotmail.com>
2020-05-09 20:32:42 +0200

    09052020

---
+++ OneToOneUni/src/com/formation/test/Test.java
@@ -0,0 +1,49 @@
+package com.formation.test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.formation.entities.Contact;
+import com.objis.demojpa.domaine.Formation;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		// 1 : Ouverture unité de travail JPA
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("BasePU");
+
+		EntityManager em = emf.createEntityManager();
+
+		// 2 : Ouverture transaction
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		// 3 : Instanciation Objet métier
+		
+//		Formation formation = new Formation("Hibernate");
+
+		Contact contact = new Contact();
+		
+		Contact contact1 = new Contact("test","test","test");
+		contact.setNom("JAVA");
+		contact.setPrenom("Formation");
+		contact.setEmail("amour-youcef@hotmail.com");
+		
+		// 4 : Persistance Objet/Relationnel : création d'un enregistrement en base
+		em.persist(contact);
+		em.persist(contact1);
+		// 5 : Fermeture transaction
+		tx.commit();
+
+		// 6 : Fermeture unité de travail JPA
+		em.close();
+		
+		emf.close();
+		
+		
+		System.out.println("ok");
+	}
+}
