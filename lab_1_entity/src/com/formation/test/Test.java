f07e338882fde468e913abf79b2eb07b3f3f9b72
dev <amour-youcef@hotmail.com>
2020-05-09 03:15:50 +0200

    09052020

--- lab_1_entity/src/com/formation/test/Test.java
+++ lab_1_entity/src/com/formation/test/Test.java
@@ -5,15 +5,24 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import com.formation.entities.Contact;
+
 public class Test {
 
 	public static void main(String[] args){
 		
-	
+	    // creation des instances
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("BasePU");
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
 		
+		Contact contact = new Contact();
+		contact.setNom("JAVA");
+		contact.setPrenom("Formation");
+		contact.setEmail("amour-youcef@hotmail.com");
+		
+		//integration avec jpa-hibernate
+		em.persist(contact);
 		
 		System.out.println("OK   §");
 	}
