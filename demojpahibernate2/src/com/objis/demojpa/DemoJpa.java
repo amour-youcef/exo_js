a022396009b6b86e13923622d408fe7292200474
dev <amour-youcef@hotmail.com>
2020-05-09 04:48:31 +0200

    09052020

--- demojpahibernate2/src/com/objis/demojpa/DemoJpa.java
+++ demojpahibernate2/src/com/objis/demojpa/DemoJpa.java
@@ -1,13 +1,11 @@
 package com.objis.demojpa;
 
-
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-
 import com.objis.demojpa.domaine.Formation;
+
 
 public class DemoJpa {
 
